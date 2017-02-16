4911208f3a59b8165446c14b973d668869300028
Johannes Peter <joshadow@gmail.com>
2017-02-16 02:46:58 -0800

    added git configs

---
+++ git_setup.sh
@@ -0,0 +1,12 @@
+# User Info
+git config --global user.email "joshadow@gmail.com"
+git config --global user.name "Johannes Peter"
+
+# Text editor
+git config --global core.editor nano
+
+# Push Size 
+git config http.postBuffer 524288000
+
+# Push mechanism
+git config --global push.default simple
